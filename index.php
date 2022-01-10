324e34eec19ccb0ece9881c3d25829ab8c9bdac9
unknown <vitor.theus62@gmail.com>
2022-01-10 01:04:23 -0300

    criamos o page admin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 
 $app = new Slim();
@@ -14,6 +15,15 @@ $app->get('/', function() {
     
 
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin1', function() {
+    
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -12,23 +12,24 @@ private $defaults = [
 
 ];
 
-public function __construct($opts = array()){
 
+public function __construct($opts = array(), $tpl_dir = "/views/"){
+ 
     $this->options = array_merge($this->defaults, $opts);
-
+ 
     $config = array(
-        "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+        "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
         "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-        "debug"         => false
-       );
-
-Tpl::configure( $config );
-
-$this->tpl = new Tpl;
-
-$this->setData($this->options["data"]);
-
-$this->tpl->draw("header");
+        "debug"         => false // set to false to improve the speed
+    );
+ 
+    Tpl::configure( $config );
+ 
+    $this->tpl = new Tpl;
+ 
+    $this->setData($this->options["data"]);
+    $this->tpl->draw("header");
+   
 }
 
 private function setData($data = array()){
